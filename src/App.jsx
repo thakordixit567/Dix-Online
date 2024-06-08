dda5aa7af3ba9bd68dd04320cd747dcdac5d9ab0
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2024-06-08 19:25:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+
+
+  return (
+    <div>
+      <h1 className="text-center mt-16 font-semibold text-2xl"> Hello, Developer's !</h1>
+  </div>
+  )
+  
+}
+
+export default App;
