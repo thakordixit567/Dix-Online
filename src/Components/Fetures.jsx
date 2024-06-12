1d4810af5a00113d58540db217519e2479fbce4a
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2024-06-12 19:53:23 +0530

    UPDATE!

--- src/Components/Fetures.jsx
+++ src/Components/Fetures.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const Fetures = () => {
-  return (
-    <div>Fetures</div>
-  )
-}
+  const [product, setProduct] = useState([]);
+  useEffect(() => {
+     const fetchProducts = async() => {
+        try {
+          const response = await fetch('/public/product.json');
+          clg
+        } catch (error) {
+          console.log(error)
+        }
+     }
+  }, []);
 
-export default Fetures
+  return (
+    <section className="bg-white py-8">
+      <h2 className="text-2xl font-semibold pl-6 pt-5 font-fortext">
+        Some Fetures Of Video Calling Service
+      </h2>
+    </section>
+  );
+};
+
+export default Fetures;
