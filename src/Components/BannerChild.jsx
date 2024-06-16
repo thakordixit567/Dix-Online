9dab91711d8c22064c8cd0cb20fd4c95de955f8f
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2024-06-16 22:05:00 +0530

    UPDATE!

--- src/Components/BannerChild.jsx
+++ src/Components/BannerChild.jsx
@@ -4,15 +4,15 @@ const BannerChild = () => {
   return (
     <section className='w-full mx-auto flex pt-12 md:pt-0 md:items-center bg-cover bg-right' 
     style={{
-      maxWidth: '1000px',
+      maxWidth: '1600px',
       height: '32rem',
-      backgroundImage: "url('https://cdn.wedevs.com/uploads/2020/06/Free-online-meeting-tools_-Which-One-to-Choose-and-Why.png')",
+      backgroundImage: "url('/box/boxpro.png')",
     }}>
     <div className=' container mx-auto'>
       <div className=' flex flex-col w-full lg:w-1/2 justify-center items-start px-6 tracking-wide mr-4'>
-      <h1 className=' text-3xl mb-6 font-bold text-white my-4 font-fortext'>Best In The Class</h1>
+      <h1 className=' text-7xl hover:text-yellow-500 text-left mb-6 font-bold text-green-700 my-4 font-fortext'>Best In The Class</h1>
       <a href="#" 
-      className=' text-xl mb-24 text-white inline-block no-underline  border-b border-gray-900 leading-relaxed hover:text-indigo-900  hover:border-black '>
+      className=' text-xl mb-24 text-black  inline-block no-underline  border-b border-gray-900 leading-relaxed hover:text-indigo-900  hover:border-black '>
       Watch Preview
       </a>
       </div>
